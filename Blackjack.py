292c73e7d5f57d82bc4c3ce73bcf831e71764649
pawik077 <pawik.r.s.077@gmail.com>
2022-11-22 00:41:48 +0100

    Working dataset generation

---
+++ Blackjack.py
@@ -0,0 +1,172 @@
+from Deck import Deck
+
+import random as rd
+from time import time
+
+deck = Deck()
+
+def handValue(hand):
+    '''Returns the value of a hand'''
+    value = 0
+    for card in hand:
+        value += card.value
+    c = 0
+    while value > 21 and c < len(hand):
+        if hand[c].rank == 'A':
+            value -= 10
+        c += 1
+    return value
+
+def round(montecarlo: bool, level: int, debug: bool):
+    '''Play a single round of blackjack
+    Args:
+        montecarlo (bool): Whether to use montecarlo simulation
+        level (int): The level of montecarlo simulation
+            level 0: gather data about the player's hand
+            level 1: gather level 1 and data about the dealer's hand
+            level 2: gather level 2 and the history of seen cards (card counting)
+        debug (bool): Whether to print debug messages'''
+    # data collection lists
+    data = []
+    tags = []
+    if level < 3: deck.shuffle() # shuffle the deck if we're not counting cards
+    if deck.cardinality() < 10: 
+        if debug: print("Shuffling deck")
+        deck.shuffle() # shuffle the deck if there are less than 10 cards left
+    choices = ['h', 's']
+
+    # initial deal
+    dealersHand = [deck.deal(), deck.deal()]
+    playersHand = [deck.deal(), deck.deal()]
+    if debug:
+        print(f"Player's hand: {playersHand}")
+        print(f"Dealer's hand: [{dealersHand[0]}, ??]")
+    
+    sum = handValue(playersHand)
+    
+    # if blackjack
+    if sum == 21 and handValue(dealersHand) == 21:
+        if debug: print("Push")
+        return data, tags
+    elif sum == 21 and handValue(dealersHand) != 21:
+        if debug: print("Blackjack! Player wins!")
+        return data, tags
+    elif sum != 21 and handValue(dealersHand) == 21:
+        if debug: print("Dealer has blackjack! Player loses!")
+        return data, tags
+    else:
+        playersSum = sum
+
+    # player's turn
+    for i in range(8):
+        if montecarlo:
+            choice = rd.choice(choices)
+        else:
+            choice = input("Hit or stand? (h/s) ")
+            while choice not in choices:
+                choice = input("Invalid input. Hit or stand? (h/s) ")
+        if choice == 'h':
+            if level == 1:
+                data.append(handValue(playersHand))
+            elif level == 2:
+                data.append([handValue(playersHand), dealersHand[0].value])
+            elif level == 3:
+                data.append([handValue(playersHand), dealersHand[0].value] + deck.negation())
+            playersHand.append(deck.deal())
+            sum = handValue(playersHand)
+            if debug:
+                print("Hitting")
+                print(f"Player's hand: {playersHand}")
+                print(f"Dealer's hand: {dealersHand[0]}, ??")
+            if sum > 21:
+                tags.append('s')
+                if debug: print("Player busts! Player loses!")
+                return data, tags
+            elif sum == 21:
+                tags.append('h')
+                if debug: print('Player wins!')
+                return data, tags
+            else:
+                tags.append('h')
+                playersSum = sum
+        else:
+            if level == 1:
+                data.append(handValue(playersHand))
+            elif level == 2:
+                data.append([handValue(playersHand), dealersHand[0].value])
+            elif level == 3:
+                data.append([handValue(playersHand), dealersHand[0].value] + deck.negation())
+            if debug:
+                print("Standing")
+                print(f"Player's hand: {playersHand}")
+                print(f"Dealer's hand: {dealersHand[0]}, ??")
+            # dealer's turn
+            while handValue(dealersHand) < 17:
+                dealersHand.append(deck.deal())
+            if debug:
+                print("Dealer hits")
+                print(f"Player's hand: {playersHand}")
+                print(f"Dealer's hand: {dealersHand}")
+            sum = handValue(dealersHand)
+            if sum > 21:
+                tags.append('s')
+                if debug: print("Dealer busts! Player wins!")
+                return data, tags
+            elif sum == 21:
+                tags.append('h')
+                if debug: print('Dealer wins!')
+                return data, tags
+            else:
+                if debug: 
+                    print("Dealer stands")
+                    print(f"Player's hand: {playersHand}")
+                    print(f"Dealer's hand: {dealersHand}")
+                if sum > playersSum:
+                    tags.append('h')
+                    if debug: print("Dealer wins!")
+                    return data, tags
+                else:
+                    tags.append('s')
+                    if debug: print("Player wins!")
+                    return data, tags
+
+def genDataSet(iters: int, output: str, level: int, shuffle: bool):
+    '''Generate a dataset of blackjack rounds
+    Args:
+        iters (int): The number of rounds to simulate
+        output (str): The name of the output file
+        level (int): The level of montecarlo simulation
+            level 0: gather data about the player's hand
+            level 1: gather level 1 and data about the dealer's hand
+            level 2: gather level 2 and the history of seen cards (card counting)
+        shuffle (bool): Whether to shuffle the deck before each round (needed only for level 3)'''
+    tStart = time()
+    for i in range(iters):
+        print(f'\rProcessing {i + 1} out of {iters}...', end='')
+        try:
+            data, tags = round(True, level, False)
+            if len(data) != len(tags):
+                print("Error: data and tags are not the same length")
+                print(data)
+                print(tags)
+                return
+            dataFile = open('datasets/' + output + '.data', 'a')
+            tagFile = open('datasets/' + output + '.tags', 'a')
+            for datum in data:
+                dataFile.write(str(datum) + '\n')
+            for tag in tags:
+                tagFile.write(tag + '\n')
+            dataFile.close()
+            tagFile.close()
+            # shuffle the deck if enabled
+            if shuffle:
+                deck.shuffle()
+        except Exception as ex:
+            print(ex)
+            deck.shuffle()
+    tEnd = time()
+    print(f'\rFinished in {(tEnd - tStart):.2f} seconds ({((tEnd - tStart) / iters * 1000):.5f} milliseconds per round)')
+
+if __name__ == '__main__':
+    # generate a dataset of 10000 rounds of blackjack
+    genDataSet(10000, 'test', 3, False)
---
+++ Deck.py
@@ -0,0 +1,78 @@
+import random
+CARD_SUIT_SYMBOLS = {"Spades": "♠", "Clubs": "♣", "Hearts": "♥", "Diamonds": "♦"}
+
+class Card:
+    def __init__(self, suit, rank):
+        '''Creates a card with the given suit and rank'''
+        self.suit = suit
+        self.rank = rank
+        self.value = self.getValue()
+        self.name = self.getName()
+    def getValue(self):
+        '''Returns the value of the card'''
+        if self.rank == 'A':
+            return 11
+        elif self.rank == 'J' or self.rank == 'Q' or self.rank == 'K':
+            return 10
+        else:
+            return int(self.rank)
+    def getName(self):
+        return self.rank + CARD_SUIT_SYMBOLS[self.suit]
+    def __repr__(self):
+        return self.name
+
+class Deck:
+    def __init__(self) -> None:
+        '''Constructor'''
+        self.cards = []
+        self.discarded = []
+        self.suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+        self.ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        self.shuffle()
+
+    def build(self) -> None:
+        '''Builds the deck'''
+        self.discarded = []
+        for s in self.suits:
+            for v in self.ranks:
+                self.cards.append(Card(s, v))
+
+    def shuffle(self) -> None:
+        '''Shuffles the deck'''
+        self.build()
+        random.shuffle(self.cards)
+
+    def cardinality(self) -> int:
+        '''Returns the number of cards in the deck'''
+        return len(self.cards)
+
+    def testDeck(self) -> None:
+        """Prints the deck to the console (for testing purposes)"""
+        for c in self.cards:
+            print(c)
+        print(len(self.cards))
+    
+    def deal(self) -> Card:
+        '''Deals a card from the deck and adds it to discarded card set'''
+        card = random.choice(self.cards)
+        self.cards.remove(card)
+        self.discarded.append(card)
+        return card
+    
+    def negation(self) -> list:
+        '''Returns the negation of the deck'''
+        final = []
+        for i in range(52):
+            final.append(0)
+        for card in self.discarded:
+            value = card.value
+            index = 4 * (value - 1)
+            if final[index] == 0:
+                final[index] = 1
+            elif final[index + 1] == 0:
+                final[index + 1] = 1
+            elif final[index + 2] == 0:
+                final[index + 2] = 1
+            elif final[index + 3] == 0:
+                final[index + 3] = 1
+        return final
